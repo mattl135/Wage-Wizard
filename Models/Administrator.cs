0ca54aad800bbf29183dd931d884139833105dfc
Matt Lockie <matt.lockie101@gmail.com>
2023-10-25 22:27:22 +1100

    Create the Administrator Class as a Sub Class of Person

---
+++ Models/Administrator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wage_Wizard.Models
+{
+    public class Administrator : Person
+    {
+        protected Administrator(
+            int id,
+            string password,
+            Title title,
+            string fName,
+            string lName,
+            string emailAddress,
+            string dob,
+            string mobileNumber,
+            int streetNumber,
+            string streetName,
+            string suburb,
+            string city,
+            string state,
+            string country
+            ) : base(
+                id,
+                password,
+                title,
+                fName,
+                lName,
+                emailAddress,
+                dob,
+                mobileNumber,
+                streetNumber,
+                streetName,
+                suburb,
+                city,
+                state,
+                country
+                )
+        {
+            //No Variables Initalised in Constructor
+        }
+    }
+}
