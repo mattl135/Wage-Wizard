6d72c462cb793135016be5908a44871c4dd4fb75
Matt Lockie <matt.lockie101@gmail.com>
2023-10-26 16:10:15 +1100

    Created Employee class and initialised constructor

---
+++ Models/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wage_Wizard.Models
+{
+    class Employee : Person
+    {
+        private int bsb {  get; set; }
+        private string accountName { get; set; }
+        private int accountNumber { get; set; }
+        private int hourlyRate {  get; set; }
+        private int taxFileNumber { get; set;}
+        private string paymentCurrencyCode {  get; set; }
+
+        protected Employee(
+            int bsb,
+            string accountName,
+            int accountNumber,
+            int hourlyRate,
+            int taxFileNumber,
+            string paymentCurrencyCode,
+            int id,
+            string password,
+            Title title,
+            string fName,
+            string lName,
+            string emailAddress,
+            string dob,
+            string mobileNumber,
+            int streetNumber,
+            string streetName,
+            string suburb,
+            string city,
+            string state,
+            string country
+            ) : base(
+                id,
+                password,
+                title,
+                fName,
+                lName,
+                emailAddress,
+                dob,
+                mobileNumber,
+                streetNumber,
+                streetName,
+                suburb,
+                city,
+                state,
+                country
+            )
+        {
+
+        }
+    }
+}
