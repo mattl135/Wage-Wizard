d6aa5a0dbaf261d9a39afb046a685e6c725c3bb8
Matt Lockie <matt.lockie101@gmail.com>
2023-10-26 16:16:32 +1100

    Created Global Settings.cs attributes

---
+++ Models/GlobalSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wage_Wizard.Models
+{
+    public class GlobalSettings
+    {
+        public static double globalTaxRate {  get; set; }
+        public static double globalSuperAnnuationRate { get; set; }
+        public static double globalLeaveRate { get; set; }
+    }
+}
