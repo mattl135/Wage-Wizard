0ea163479436caf4c718bb7f41437647942b442e
Matt Lockie <matt.lockie101@gmail.com>
2023-10-27 22:35:38 +1100

    Created Utilities Class

---
+++ Utilities/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wage_Wizard.Utilities
+{
+    public class Utilities
+    {
+        //Really usefull stuff in here!
+    }
+}
