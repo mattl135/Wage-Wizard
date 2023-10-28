878bb05fdaf7cfe2dd53f7c3b02c2913226f6e2d
Thomas <thomas.t.katsoulotos@student.uts.edu.au>
2023-10-28 17:53:02 +1100

    Admin Pay/Leave Menus setup

---
+++ Views/AdminPayApproval.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Wage_Wizard.Views
+{
+    partial class AdminPayApproval
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            employeeBindingSource = new BindingSource(components);
+            exitBtn = new Button();
+            denyBtn = new Button();
+            approveBtn = new Button();
+            payRequestsDGV = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)employeeBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)payRequestsDGV).BeginInit();
+            SuspendLayout();
+            // 
+            // employeeBindingSource
+            // 
+            employeeBindingSource.DataSource = typeof(Models.Employee);
+            // 
+            // exitBtn
+            // 
+            exitBtn.Location = new Point(756, 421);
+            exitBtn.Name = "exitBtn";
+            exitBtn.Size = new Size(86, 61);
+            exitBtn.TabIndex = 10;
+            exitBtn.Text = "Exit";
+            exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // denyBtn
+            // 
+            denyBtn.Location = new Point(150, 421);
+            denyBtn.Name = "denyBtn";
+            denyBtn.Size = new Size(86, 61);
+            denyBtn.TabIndex = 9;
+            denyBtn.Text = "Deny";
+            denyBtn.UseVisualStyleBackColor = true;
+            // 
+            // approveBtn
+            // 
+            approveBtn.Location = new Point(30, 421);
+            approveBtn.Name = "approveBtn";
+            approveBtn.Size = new Size(86, 61);
+            approveBtn.TabIndex = 8;
+            approveBtn.Text = "Approve";
+            approveBtn.UseVisualStyleBackColor = true;
+            // 
+            // payRequestsDGV
+            // 
+            payRequestsDGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            payRequestsDGV.Location = new Point(11, 18);
+            payRequestsDGV.Name = "payRequestsDGV";
+            payRequestsDGV.RowTemplate.Height = 25;
+            payRequestsDGV.Size = new Size(852, 378);
+            payRequestsDGV.TabIndex = 7;
+            // 
+            // AdminPayApproval
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(874, 500);
+            Controls.Add(exitBtn);
+            Controls.Add(denyBtn);
+            Controls.Add(approveBtn);
+            Controls.Add(payRequestsDGV);
+            Name = "AdminPayApproval";
+            Text = "AdminPayApproval";
+            ((System.ComponentModel.ISupportInitialize)employeeBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)payRequestsDGV).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private BindingSource employeeBindingSource;
+        private Button exitBtn;
+        private Button denyBtn;
+        private Button approveBtn;
+        private DataGridView payRequestsDGV;
+    }
+}
