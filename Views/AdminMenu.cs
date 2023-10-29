b2c7d32ed310c65c5484af110ee96ca58503b274
Matt Lockie <matt.lockie101@gmail.com>
2023-10-29 16:50:49 +1100

    UI Tidy - Admin View

--- Views/AdminMenu.cs
+++ Views/AdminMenu.cs
@@ -77,11 +77,25 @@ namespace Wage_Wizard.Views
 
         private void LogoutBtn_Click(object sender, EventArgs e)
         {
-            this.Close();
-            Utilities.Utilities.currentUserId = 0;
+            DialogResult dialogResult = MessageBox.Show(
+                "Are you sure you want to logout?",
+                "Logout?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,  // This sets the icon to a question mark
+                MessageBoxDefaultButton.Button2);
 
-            Login login = new Login();
-            login.Show();
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+                Utilities.Utilities.currentUserId = 0;
+
+                Login login = new Login();
+                login.Show();
+            }
+            else
+            {
+                // User clicked 'No', do nothing
+            }
         }
     }
 }
